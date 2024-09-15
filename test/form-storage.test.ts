03b14499b2ac3d6353213c6fc632a9e68036e957
Jesus Romero <hebrerillo@gmail.com>
2024-09-15 18:49:54 +0200

    First unit test

--- test/form-storage.test.ts
+++ test/form-storage.test.ts
@@ -3,11 +3,12 @@ import form_html from "./fixtures/form";
 
 describe("FormStorage", () => {
   const container = document.createElement("div");
+  const formId = "form_contact";
   container.innerHTML = form_html;
   document.body.appendChild(container);
-  const formStorage: FormStorage = new FormStorage("#form_contact");
+  const formStorage: FormStorage = new FormStorage(formId);
 
-  it("typing on a form saves data to storage", () => {
-    console.log(formStorage);
+  it("Trying to access the form data before the timeout to store should return an empty result", () => {
+    
   });
 });
