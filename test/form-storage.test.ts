3aeda7a55ebb7c61a700ad4547655b731edbd45d
Jesus Romero <hebrerillo@gmail.com>
2024-09-22 12:22:37 +0200

    Testing the form is saved on session storage

--- test/form-storage.test.ts
+++ test/form-storage.test.ts
@@ -1,27 +1,47 @@
-import {FormStorage, FormStorageItem} from "form/form-storage";
+import { FormStorageList, FormStorage } from "form/form-storage";
 import form_html from "./fixtures/form";
+
+/**
+ * Triggers a custom input event on a form.
+ */
+function triggerInputEventOnForm(formElement: HTMLFormElement): void {
+  const event = new CustomEvent("input");
+  formElement.dispatchEvent(event);
+}
 
 describe("FormStorage", () => {
   const container = document.createElement("div");
   container.innerHTML = form_html;
   document.body.appendChild(container);
-  const formId = "form_contact";
-  const formElement = document.querySelector(
-    `#${formId}`,
-  ) as HTMLFormElement;
+  const formElement = document.querySelector("form") as HTMLFormElement;
   new FormStorage(formElement);
-  sessionStorage.setItem(formId, "");
+  
+  beforeEach(() => {
+    //Clean storage before each test
+    sessionStorage.setItem(formElement.id, "");
+  });
+
 
   it("After typing some input to the form, the information is saved in storage", () => {
     (<HTMLInputElement>formElement.querySelector('[name="firstName"]')).value = "Hannibal";
 
-    const event = new CustomEvent("input");
-    formElement.dispatchEvent(event);
-    const storedString = sessionStorage.getItem(formId)!;
+    triggerInputEventOnForm(formElement);
+    const storedString = sessionStorage.getItem(formElement.id)!;
     expect(storedString).not.toBe(null);
     expect(storedString.length).toBeGreaterThan(0);
 
-    const storeInfo: Array<FormStorageItem> = JSON.parse(storedString);
-    expect(storeInfo.length).toBeGreaterThan(0);
+    const storeInfo = {list: []} as FormStorageList;
+    storeInfo.list = JSON.parse(storedString);
+    expect(storeInfo.list.length).toBeGreaterThan(0);
+
+    let found: boolean = false;
+
+    for(let i = 0; i < storeInfo.list.length && !found; ++i) {
+      if (storeInfo.list[i].name === "firstName") {
+        found = true;
+        expect(storeInfo.list[i].value).toBe("Hannibal");
+      }
+    }
+    expect(found).toBe(true);
   });
 });
