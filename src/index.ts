5d6a59ce0e048f3b96267b3de15512cbbf1aad5f
Jesus Romero <jesus.romero@techpump.com>
2024-11-26 08:05:43 +0100

    Doing refactor of index

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,24 @@
 import "../styles/sass/main.scss";
 import { FormStorage } from "src/form/form-storage";
 
-document.addEventListener("DOMContentLoaded", () => {
-  new FormStorage(document.querySelector("#form_contact"));
-});
+(function () {
+  class App {
+    private form: HTMLFormElement | null;
+
+    constructor() {
+      this.form = null;
+      if (document.readyState === "loading") {
+        document.addEventListener("DOMContentLoaded", this.init.bind(this));
+      } else {
+        this.init();
+      }
+    }
+
+    private init() {
+      this.form = document.querySelector("#form_contact");
+      new FormStorage(this.form);
+    }
+  }
+
+  new App();
+})();
